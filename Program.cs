f1bd76e10bce7f6faf3ddefbfadfd7e135564e7f
xluigyx <rvl5000341@est.univalle.edu>
2023-05-10 16:04:32 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security;
+
+namespace grupo_d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int result = sun(2, 3);
+        }
+        public static int sun(int n1, int n2)
+        {
+            return n1+n2;
+        }
+    }
+}
