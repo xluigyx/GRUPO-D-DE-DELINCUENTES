9987ec171d6352d159d3a33502e66a03472bdd94
xluigyx <rvl5000341@est.univalle.edu>
2023-05-10 16:13:52 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -8,10 +8,15 @@ namespace grupo_d
         {
             Console.WriteLine("Hello, World!");
             int result = sun(2, 3);
+            string Nombrecompleto = nombrecompleto("luis", "ramirez");
         }
         public static int sun(int n1, int n2)
         {
             return n1+n2;
+        }
+        public static string nombrecompleto(string name, string lastname)
+        {
+            return name+lastname;
         }
     }
 }
